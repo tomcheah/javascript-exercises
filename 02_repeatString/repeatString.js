69de8d91eac10e911369f82a280be4ad3a4253a9
Tom Cheah <tomcheah@berkeley.edu>
2022-05-22 23:34:06 -0700

    Finish exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    } 
 
+    let ret = "";
+    for (let i = 0; i < times; i++) {
+        ret += word;
+    }
+    return ret;
 };
 
 // Do not edit below this line
